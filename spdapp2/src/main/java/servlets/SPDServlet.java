992e2d2264044f359d2bd1f0b00cb0f996bd66c4
apollo.saifullin@gmail.com <apollo.saifullin@gmail.com>
2016-12-26 00:46:58 +0200

    Added code for delete row from DB

--- spdapp2/src/main/java/servlets/SPDServlet.java
+++ spdapp2/src/main/java/servlets/SPDServlet.java
@@ -11,6 +11,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import beans.SPD;
+import dao.SPDDAO;
 import repositories.SPDDaoImpl;
 
 @WebServlet("/spd")
@@ -21,33 +22,61 @@ public class SPDServlet extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		if (request.getParameter("add") != null) {
-			request.getRequestDispatcher("jsp/addSPD.jsp").forward(request, response);
-		} else {
-			int id = Integer.parseInt(request.getParameter("id"));
-			try {
+		request.setCharacterEncoding("UTF-8");
+		try {
+			if (request.getParameter("add") != null) {
+				request.getRequestDispatcher("jsp/addSPD.jsp").forward(request, response);
+			} else {
+				int id = Integer.parseInt(request.getParameter("id"));
 				SPD spd = spdDao.selectById(id);
-			} catch (SQLException e) {
-				throw new ServletException(e);
+				request.setAttribute("spd", spd);
+				if (request.getParameter("edit") != null) {
+					request.getRequestDispatcher("jsp/editSPD.jsp").forward(request, response);
+				} else {
+					request.getRequestDispatcher("jsp/viewSPD.jsp").forward(request, response);
+				}
 			}
+		} catch (SQLException e) {
+			throw new ServletException(e);
 		}
+
 	}
 
 	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		if (request.getParameter("add") != null) {
-			try {
+		request.setCharacterEncoding("UTF-8");
+		try {
+			if (request.getParameter("add") != null) {
+
 				SPD spd = new SPD(request.getParameter("surname"), request.getParameter("firstname"),
 						request.getParameter("lastname"), request.getParameter("alias"), request.getParameter("inn"),
 						request.getParameter("passport"));
 				spdDao.create(spd);
-			} catch (SQLException e) {
-				throw new ServletException(e);
+				response.sendRedirect("spd?id=" + spd.getId());
+			} else if (request.getParameter("edit") != null) {
+				int id = Integer.parseInt(request.getParameter("id"));
+				SPD spd = spdDao.selectById(id);
+				spd.setSurname(request.getParameter("surname"));
+				spd.setFirstname(request.getParameter("firstname"));
+				spd.setLastname(request.getParameter("lastname"));
+				spd.setAlias(request.getParameter("alias"));
+				spd.setInn(request.getParameter("inn"));
+				spd.setPassport(request.getParameter("passport"));
+				spdDao.update(spd);
+				response.sendRedirect("spd?id=" + spd.getId());
+			} else if (request.getParameter("delete") != null) {
+				int id = Integer.parseInt(request.getParameter("id"));
+				SPD spd = spdDao.selectById(id);
+				spdDao.delete(spd);
+				response.sendRedirect("listAllSPD");
+			} else {
+				super.doPost(request, response);
 			}
-			response.sendRedirect("listAllSPD");
-
+		} catch (SQLException e) {
+			throw new ServletException(e);
 		}
+
 	}
 
 }
