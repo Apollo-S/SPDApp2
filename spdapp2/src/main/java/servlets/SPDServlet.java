c1a48c879c6267314f51f7149dfdf5caeec26cdf
apollo.saifullin@gmail.com <apollo.saifullin@gmail.com>
2017-01-09 23:30:58 +0200

    Added deleting with cascade of all rows which were related with SPD

--- spdapp2/src/main/java/servlets/SPDServlet.java
+++ spdapp2/src/main/java/servlets/SPDServlet.java
@@ -12,11 +12,13 @@ import javax.servlet.http.HttpServletResponse;
 import beans.Account;
 import beans.Address;
 import beans.Agreement;
+import beans.AgreementTarif;
 import beans.RegistrationInfo;
 import beans.SPD;
 import repositories.AccountDAOImpl;
 import repositories.AddressDAOImpl;
 import repositories.AgreementDAOImpl;
+import repositories.AgreementTarifDAOImpl;
 import repositories.RegistrationInfoDAOImpl;
 import repositories.SPDDAOImpl;
 
@@ -28,6 +30,7 @@ public class SPDServlet extends HttpServlet {
 	private final RegistrationInfoDAOImpl regInfoDao = new RegistrationInfoDAOImpl();
 	private final AccountDAOImpl accountDao = new AccountDAOImpl();
 	private final AgreementDAOImpl agreementDao = new AgreementDAOImpl();
+	private final AgreementTarifDAOImpl tarifDao = new AgreementTarifDAOImpl();
 
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
@@ -48,9 +51,9 @@ public class SPDServlet extends HttpServlet {
 				request.setAttribute("regInfo", regInfo);
 				request.setAttribute("accounts", accounts);
 				request.setAttribute("agreements", agreements);
-				if (request.getParameter("edit") != null) { // get edit row
+				if (request.getParameter("edit") != null) {
 					request.getRequestDispatcher("jsp/editSPD.jsp").forward(request, response);
-				} else { // get view row
+				} else {
 					request.getRequestDispatcher("jsp/viewSPD.jsp").forward(request, response);
 				}
 			}
@@ -63,7 +66,7 @@ public class SPDServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		request.setCharacterEncoding("UTF-8");
-		try { // post add row
+		try {
 			if (request.getParameter("add") != null) {
 				Address address = new Address(request.getParameter("country"), request.getParameter("region"),
 						request.getParameter("city"), request.getParameter("street"), request.getParameter("building"),
@@ -78,7 +81,7 @@ public class SPDServlet extends HttpServlet {
 						request.getParameter("passport"), address.getId(), regInfo.getId());
 				spdDao.create(spd);
 				response.sendRedirect("spd?id=" + spd.getId());
-			} else if (request.getParameter("edit") != null) { // post edit row
+			} else if (request.getParameter("edit") != null) {
 				int id = Integer.parseInt(request.getParameter("id"));
 				SPD spd = spdDao.selectById(id);
 				Address address = addressDao.selectById(spd.getAddressId());
@@ -106,10 +109,18 @@ public class SPDServlet extends HttpServlet {
 				spdDao.update(spd);
 				response.sendRedirect("spd?id=" + spd.getId());
 			} else if (request.getParameter("delete") != null) { // post delete row
-				int id = Integer.parseInt(request.getParameter("id"));
-				SPD spd = spdDao.selectById(id);
+				int spdId = Integer.parseInt(request.getParameter("id"));
+				SPD spd = spdDao.selectById(spdId);
 				Address address = addressDao.selectById(spd.getAddressId());
 				RegistrationInfo regInfo = regInfoDao.selectById(spd.getRegistrationInfoId());
+				List<Agreement> agreements = agreementDao.selectAllBySPDId(spdId);
+				for(Agreement agreement : agreements) {
+					List<AgreementTarif> tarifs = tarifDao.selectAllByAgreementId(agreement.getId());
+					for(AgreementTarif tarif : tarifs) {
+						tarifDao.delete(tarif);
+					}
+					agreementDao.delete(agreement);
+				}
 				addressDao.delete(address);
 				regInfoDao.delete(regInfo);
 				spdDao.delete(spd);
