63a73e6008a8454fda4a7678a17206bc4333c3e6
apollo.saifullin@gmail.com <apollo.saifullin@gmail.com>
2017-01-26 17:10:57 +0200

    create() method changed to void

--- spdapp2/src/main/java/dao/PaymentDAO.java
+++ spdapp2/src/main/java/dao/PaymentDAO.java
@@ -6,7 +6,7 @@ import java.util.List;
 import entity.Payment;
 
 public interface PaymentDAO {
-	public int create(Payment payment) throws SQLException;
+	public void create(Payment payment) throws SQLException;
 	public boolean delete(Payment payment) throws SQLException;
 	public boolean update(Payment payment) throws SQLException;
 	public Payment selectById(int id) throws SQLException;
