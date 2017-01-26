dcb863d8d34a89b5dabc4f4c4931fe1498be8208
apollo.saifullin@gmail.com <apollo.saifullin@gmail.com>
2017-01-26 17:10:07 +0200

    Attribute name dateFinal changed to dateFinish

--- spdapp2/src/main/java/entity/Payment.java
+++ spdapp2/src/main/java/entity/Payment.java
@@ -11,17 +11,17 @@ public class Payment implements Serializable {
 	private int paymentTypeId;
 	private double value;
 	private Date dateStart;
-	private Date dateFinal;
+	private Date dateFinish;
 
 	public Payment() {
 	}
 
-	public Payment(int spdId, int paymentTypeId, double value, Date dateStart, Date dateFinal) {
+	public Payment(int spdId, int paymentTypeId, double value, Date dateStart, Date dateFinish) {
 		this.spdId = spdId;
 		this.paymentTypeId = paymentTypeId;
 		this.value = value;
 		this.dateStart = dateStart;
-		this.dateFinal = dateFinal;
+		this.dateFinish = dateFinish;
 	}
 
 	public int getId() {
@@ -64,12 +64,12 @@ public class Payment implements Serializable {
 		this.dateStart = dateStart;
 	}
 
-	public Date getDateFinal() {
-		return dateFinal;
+	public Date getDateFinish() {
+		return dateFinish;
 	}
 
-	public void setDateFinal(Date dateFinal) {
-		this.dateFinal = dateFinal;
+	public void setDateFinish(Date dateFinish) {
+		this.dateFinish = dateFinish;
 	}
 
 	public static long getSerialversionuid() {
