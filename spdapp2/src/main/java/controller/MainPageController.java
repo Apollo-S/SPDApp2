e9b48736712392deeead966aaf0efe59413c0d7a
apollo.saifullin@gmail.com <apollo.saifullin@gmail.com>
2017-02-03 15:06:54 +0200

    springified MainPageController

--- spdapp2/src/main/java/controller/MainPageController.java
+++ spdapp2/src/main/java/controller/MainPageController.java
@@ -1,18 +1,15 @@
 package controller;
 
-import java.io.IOException;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
-@WebServlet("/main")
-public class MainPageController extends HttpServlet {
-	private static final long serialVersionUID = 1L;
+@Controller
+public class MainPageController {
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		request.getRequestDispatcher("jsp/main.jsp").forward(request, response);
+	@RequestMapping(value = "main", method = RequestMethod.GET)
+	public String getMainPage() {
+		return "main";
 	}
 	
 }
