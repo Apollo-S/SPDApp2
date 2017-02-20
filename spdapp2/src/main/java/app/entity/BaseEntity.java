c82756a15b07aa8101cd653f2dc00db71bc3074f
apollo.saifullin@gmail.com <apollo.saifullin@gmail.com>
2017-02-20 12:04:32 +0200

    new field 'version' in BaseEntity, marked as @Version

--- spdapp2/src/main/java/app/entity/BaseEntity.java
+++ spdapp2/src/main/java/app/entity/BaseEntity.java
@@ -3,6 +3,7 @@ package app.entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
+import javax.persistence.Version;
 
 @MappedSuperclass
 public abstract class BaseEntity {
@@ -11,6 +12,9 @@ public abstract class BaseEntity {
 	@GeneratedValue
 	private Integer id;
 
+	@Version
+	private Integer version;
+
 	public Integer getId() {
 		return id;
 	}
@@ -18,7 +22,15 @@ public abstract class BaseEntity {
 	public void setId(Integer id) {
 		this.id = id;
 	}
-	
+
+	public Integer getVersion() {
+		return version;
+	}
+
+	public void setVersion(Integer version) {
+		this.version = version;
+	}
+
 	public abstract String getUrl();
-	
+
 }
