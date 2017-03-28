52724409578da8e3892cbbc892c41357ce98f620
apollo.saifullin@gmail.com <apollo.saifullin@gmail.com>
2017-03-28 15:48:04 +0300

    css & js moved in webapp/resource folder (for using by spring container)

---
+++ spdapp2/src/main/webapp/resources/js/scripts.js
@@ -0,0 +1,36 @@
+﻿/* Find any element which has a 'data-onload' function and load that to simulate an onload. */
+$('[data-onload]').each(function() {
+	eval($(this).data('onload'));
+});
+
+$( function() {
+	$("#date").datepicker({
+		format: "dd-mm-yyyy",
+		weekStart: 1,
+		clearBtn: true,
+		calendarWeeks: true,
+		autoclose: true,
+		todayHighlight: true
+	});
+});
+
+function calcSpecificationSum() {
+	var configuringTarif = +document.getElementById("configuringHoursTarif").value;
+	var programmingTarif = +document.getElementById("programmingHoursTarif").value;
+	var architectingTarif = +document.getElementById("architectingHoursTarif").value;
+	var configuring = +document.getElementById("configuringHours").value;
+	var programming = +document.getElementById("programmingHours").value;
+	var architecting = +document.getElementById("architectingHours").value;
+	var hours = parseInt(configuring + programming + architecting);
+	var days = +hours / 8;
+	var specSum = (configuring * configuringTarif)
+			+ (programming * programmingTarif)
+			+ (architecting * architectingTarif);
+	var finalLabel = document.getElementById('sum');
+	finalLabel.innerHTML = '<b>' + "Итого: " + specSum.toFixed(2) + " грн | " + hours + " час. | " + days.toFixed(0) + " дн." + '</b>';
+	document.getElementById("sum").bold();
+}
+
+function goToAddress(url) {
+	window.location.href = url;
+}
