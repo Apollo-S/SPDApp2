bc1f3665a3c3e12f942f9cb0dbdd6ac30ddf4dbb
apollo.saifullin@gmail.com <apollo.saifullin@gmail.com>
2017-05-07 09:51:02 +0300

    fixes in 'convertStringToDouble' method added for better converting

--- spdapp2/src/main/java/utils/BeanUtil.java
+++ spdapp2/src/main/java/utils/BeanUtil.java
@@ -1,31 +1,32 @@
 package utils;
 
 import java.sql.Date;
+import java.util.Locale;
 
 public class BeanUtil {
 	
 	private static final char MINUS = '-';
-	private static final String NON_SPACE = "";
-	private static final String SPACE = "\\s";
-	private static final String DOT = ".";
-	private static final String COMMA = ",";
+	private static final String EMPTY = "";
+	private static final String EXCEPT_DIGITS_AND_DOT = "[^0-9.]";
+	private static final char DOT = '.';
+	private static final char COMMA = ',';
 
 	public static Date requestedDateFormatter(String requestedDate) {
-		if (requestedDate.equals(NON_SPACE))  {
+		if (requestedDate.equals(EMPTY))  {
 			return null;
 		}
 		return Date.valueOf(requestedDate);
 	}
 	
 	public static Double convertStringToDouble(String parameter) {
-		if (parameter.equals(NON_SPACE)) {
+		if (parameter.equals(EMPTY)) {
 			return null;
 		}
-		parameter = parameter.replace(COMMA, DOT).replaceAll(SPACE, NON_SPACE);
-		if (!BeanUtil.isStringNumeric(parameter)) {
-			return null;
+		parameter = parameter.replace(COMMA, DOT).replaceAll(EXCEPT_DIGITS_AND_DOT, EMPTY);
+		if (BeanUtil.isStringNumeric(parameter)) {
+			return Double.valueOf(parameter);
 		}
-		return Double.parseDouble(parameter.replace(COMMA, DOT).replaceAll(SPACE, NON_SPACE));
+		return null;
 	}
 	
 	public static boolean isStringNumeric(String str) {
@@ -35,7 +36,7 @@ public class BeanUtil {
 			return false; 
 		}
 		boolean isDecimalSeparatorFound = false;
-		char localeDecimalSeparator = '.';
+		char localeDecimalSeparator = DOT;
 		for (char c : str.substring(1).toCharArray()) {
 			if (!Character.isDigit(c)) {
 				if (c == localeDecimalSeparator && !isDecimalSeparatorFound) {
--- spdapp2/src/test/java/utils/BeanUtilTest.java
+++ spdapp2/src/test/java/utils/BeanUtilTest.java
@@ -9,55 +9,53 @@ import java.sql.Date;
 
 public class BeanUtilTest {
 	
-	BeanUtil beanUtils = new BeanUtil();
-	
 	@Test
 	public void testRequestedDateFormatterWithTextDate() {
 		String textDate = "2017-01-01";
 		Date sqlDate = Date.valueOf(textDate);
-		assertThat(beanUtils.requestedDateFormatter(textDate), is(sqlDate));
+		assertThat(BeanUtil.requestedDateFormatter(textDate), is(sqlDate));
 	}
 	
 	@Test
 	public void testRequestedDateFormatterWithEmptyValue() {
 		String textDate = "";
 		Date sqlDate = null;
-		assertThat(beanUtils.requestedDateFormatter(textDate), is(sqlDate));
+		assertThat(BeanUtil.requestedDateFormatter(textDate), is(sqlDate));
 	}
 	
 	@Test
 	public void testConvertStringToDoubleEmptyValue() {
 		String value = "";
 		Double result = null;
-		assertThat(beanUtils.convertStringToDouble(value), is(result));
+		assertThat(BeanUtil.convertStringToDouble(value), is(result));
 	}
 	
 	@Test
 	public void testConvertStringToDoubleWithDigits() {
 		String value = "7 200,89";
-		Double result = 7200.89d;
-		assertThat(beanUtils.convertStringToDouble(value), is(result));
+		Double result = 7200.89;
+		assertThat(BeanUtil.convertStringToDouble(value), is(result));
 	}
 	
 	@Test
 	public void testConvertStringToDoubleWithLetters() {
 		String value = "23frg";
 		Double result = null;
-		assertThat(beanUtils.convertStringToDouble(value), is(result));
+		assertThat(BeanUtil.convertStringToDouble(value), is(result));
 	}
 	
 	@Test
 	public void testConvertStringToDoubleWithMinus() {
 		String value = "-7 200,89";
-		Double result = -7200.89d;
-		assertThat(beanUtils.convertStringToDouble(value), is(result));
+		Double result = -7200.89;
+		assertThat(BeanUtil.convertStringToDouble(value), is(result));
 	}
 	
 	@Test
 	public void testConvertStringToDoubleWithDigits2() {
 		String value = "7200.89";
-		Double result = 7200.89d;
-		assertThat(beanUtils.convertStringToDouble(value), is(result));
+		Double result = 7200.89;
+		assertThat(BeanUtil.convertStringToDouble(value), is(result));
 	}
 	
 }
