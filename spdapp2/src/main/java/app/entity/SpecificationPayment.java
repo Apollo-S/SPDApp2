768ee3ce49e9511702826408d77daded94c336e1
apollo.saifullin@gmail.com <apollo.saifullin@gmail.com>
2017-06-14 17:11:50 +0300

    new POJO for Specification view

---
+++ spdapp2/src/main/java/app/entity/SpecificationPayment.java
@@ -0,0 +1,85 @@
+package app.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "specification_payment")
+public class SpecificationPayment extends UrlEntity implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@ManyToOne
+	@JoinColumn(name = "specification_id")
+	private Specification specification;
+
+	@Column(name = "payment_number")
+	private Integer paymentNumber;
+
+	@Column(name = "payment_sum")
+	private Double paymentSum;
+
+	@Column(name = "payment_days")
+	private Integer paymentDays;
+
+	@Column(name = "comment")
+	private String comment;
+
+	public SpecificationPayment() {
+	}
+
+	public SpecificationPayment(Specification specification, Integer paymentNumber, Double paymentSum,
+			Integer paymentDays, String comment) {
+		this.specification = specification;
+		this.paymentNumber = paymentNumber;
+		this.paymentSum = paymentSum;
+		this.paymentDays = paymentDays;
+		this.comment = comment;
+	}
+
+	public Specification getSpecification() {
+		return specification;
+	}
+
+	public void setSpecification(Specification specification) {
+		this.specification = specification;
+	}
+
+	public Integer getPaymentNumber() {
+		return paymentNumber;
+	}
+
+	public void setPaymentNumber(Integer paymentNumber) {
+		this.paymentNumber = paymentNumber;
+	}
+
+	public Double getPaymentSum() {
+		return paymentSum;
+	}
+
+	public void setPaymentSum(Double paymentSum) {
+		this.paymentSum = paymentSum;
+	}
+
+	public Integer getPaymentDays() {
+		return paymentDays;
+	}
+
+	public void setPaymentDays(Integer paymentDays) {
+		this.paymentDays = paymentDays;
+	}
+
+	public String getComment() {
+		return comment;
+	}
+
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
+
+}
