dbd2f3c2c39242d859002caeb6797dda44b1723b
apollo.saifullin@gmail.com <apollo.saifullin@gmail.com>
2017-07-04 12:39:55 +0300

    experimental Converter for Dates, need to finalize it

---
+++ spdapp2/src/main/java/converter/DateConverter.java
@@ -0,0 +1,15 @@
+package converter;
+
+import java.util.Date;
+
+import org.springframework.core.convert.converter.Converter;
+
+public class DateConverter implements Converter<String, Date> {
+
+	@Override
+	public Date convert(String date) {
+		// TODO make convertion of date
+		return null;
+	}
+
+}
