bfa1be81bd46ae0f7043adb25ccfae6774e96d32
apollo.saifullin@gmail.com <apollo.saifullin@gmail.com>
2017-07-26 02:39:34 +0300

    added classes that was needed for authorization of user through database

---
+++ spdapp2/src/main/java/app/entity/CustomUser.java
@@ -0,0 +1,127 @@
+package app.entity;
+
+import java.io.Serializable;
+import java.util.Collection;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table(name = "user")
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+public class CustomUser extends UrlEntity implements UserDetails, Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Column(name = "first_name", length = 30)
+	private String firstName;
+
+	@Column(name = "last_name", length = 30)
+	private String lastName;
+
+	@Column(name = "username", length = 25)
+	private String username;
+
+	@Column(name = "password", length = 25)
+	private String password;
+
+	@Column(name = "email", length = 25)
+	private String email;
+
+	@Column(name = "role", length = 25)
+	private String role;
+
+	public CustomUser() {
+	}
+
+	public CustomUser(String firstName, String lastName, String username, String password, String email, String role) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.username = username;
+		this.password = password;
+		this.email = email;
+		this.role = role;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return AuthorityUtils.createAuthorityList(this.role);
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}
+
+}
---
+++ spdapp2/src/main/java/app/service/CustomUserDetailsService.java
@@ -0,0 +1,27 @@
+package app.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+import app.entity.CustomUser;
+import app.repository.CustomUserRepository;
+
+@Component("customUserDetailsService")
+public class CustomUserDetailsService implements UserDetailsService {
+
+	@Autowired(required = true)
+	private CustomUserRepository userRepository;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		CustomUser user = userRepository.findByUsername(username);
+		return new User(user.getUsername(), user.getPassword(), 
+				AuthorityUtils.createAuthorityList(user.getRole()));
+	}
+
+}
