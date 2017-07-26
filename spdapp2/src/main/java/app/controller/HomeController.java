b44893cdb01d23d19545d796b8e0dd013dd7b102
apollo.saifullin@gmail.com <apollo.saifullin@gmail.com>
2017-07-26 20:36:30 +0300

    new jsp page for register, new methods (GET, POST) for registration in controller

--- spdapp2/src/main/java/app/controller/HomeController.java
+++ spdapp2/src/main/java/app/controller/HomeController.java
@@ -1,14 +1,42 @@
 package app.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import app.entity.CustomUser;
+import app.repository.CustomUserRepository;
 
 @Controller
 @RequestMapping(value = "/")
 public class HomeController {
 
-	@RequestMapping(value = {"/", "main"}, method = RequestMethod.GET)
+	private static final String PARAM_ROLE_USER = "ROLE_USER";
+
+	@Autowired(required = true)
+	private CustomUserRepository userRepository;
+
+	@RequestMapping(value = { "/register" }, method = RequestMethod.POST)
+	public String register(@ModelAttribute CustomUser user) {
+		user.setRole(PARAM_ROLE_USER);
+		user = userRepository.save(user);
+		Authentication authentication = new UsernamePasswordAuthenticationToken(user, 
+				user.getPassword(), user.getAuthorities());
+		SecurityContextHolder.getContext().setAuthentication(authentication);
+		return "redirect:/";
+	}
+
+	@RequestMapping(value = { "/register" }, method = RequestMethod.GET)
+	public String goRegister() {
+		return "register";
+	}
+
+	@RequestMapping(value = { "/", "main" }, method = RequestMethod.GET)
 	public String goHome() {
 		return "main";
 	}
@@ -17,5 +45,5 @@ public class HomeController {
 	public String goLogin() {
 		return "login";
 	}
-	
+
 }
