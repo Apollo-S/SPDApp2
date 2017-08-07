0edaf79fcbddcac776af1d56ec0bac3697e23deb
apollo.saifullin@gmail.com <apollo.saifullin@gmail.com>
2017-08-07 13:42:08 +0300

    added BaseController (for constants)

---
+++ spdapp2/src/main/java/app/controller/BaseController.java
@@ -0,0 +1,15 @@
+package app.controller;
+
+public abstract class BaseController {
+	
+	static final String PARAM_DELETE = "delete";
+	static final String PARAM_EDIT = "edit";
+	static final String PARAM_ADD = "add";
+	
+	static final String REQUEST_MAPPING_AGREEMENT_TARIF = "/agreementTarif";
+	static final String REQUEST_MAPPING_AGREEMENT = "/agreement";
+	
+	static final String ROLE_ADMIN = "ROLE_ADMIN";
+	static final String ROLE_USER = "ROLE_USER";
+
+}
