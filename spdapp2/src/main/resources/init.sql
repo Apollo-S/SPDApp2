82fd31d94c846fadfdd627108bd3e9bd80f59360
apollo.saifullin@gmail.com <apollo.saifullin@gmail.com>
2017-08-15 01:01:54 +0300

    some modifications in sql-script

--- spdapp2/src/main/resources/init.sql
+++ spdapp2/src/main/resources/init.sql
@@ -1,61 +1,45 @@
+drop table if exists `user_roles`;
 drop table if exists `authorities`;
 drop table if exists `users`;
 drop table if exists `group_members`;
 drop table if exists `group_authorities`;
 drop table if exists `groups`;
 
-create table `users` (
-	`username` varchar(50) NOT NULL PRIMARY KEY,
-	`password` varchar(150) NOT NULL,
-   `enabled` boolean NOT NULL
+-- drop table if exists `persistent_logins`;
+
+CREATE  TABLE users (
+  username VARCHAR(45) NOT NULL ,
+  password VARCHAR(150) NOT NULL ,
+  enabled TINYINT NOT NULL DEFAULT 1 ,
+  PRIMARY KEY (username)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-create table `authorities` (
-	`username` varchar(50) NOT NULL,
-	`authority` varchar(50) NOT NULL,
-   CONSTRAINT `fk_authorities_users` FOREIGN KEY (`username`) REFERENCES `users`(`username`)
+CREATE TABLE user_roles (
+  user_role_id int(11) NOT NULL AUTO_INCREMENT,
+  username varchar(45) NOT NULL,
+  role varchar(45) NOT NULL,
+  PRIMARY KEY (user_role_id),
+  UNIQUE KEY uni_username_role (role,username),
+  KEY fk_username_idx (username),
+  CONSTRAINT fk_username FOREIGN KEY (username) REFERENCES users (username)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-create unique index ix_auth_username on authorities (username, authority);
-
-create table `groups` (
-	id bigint UNSIGNED NOT NULL PRIMARY KEY  AUTO_INCREMENT,
-	group_name varchar(50) not null
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-create table `group_authorities` (
-	group_id bigint UNSIGNED not null,
-	authority varchar(50) not null,
-	constraint fk_group_authorities_group foreign key(group_id) references groups(id)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-create table `group_members` (
-	id bigint UNSIGNED NOT NULL PRIMARY KEY  AUTO_INCREMENT,
-	username varchar(50) not null,
-	group_id bigint UNSIGNED not null,
-	constraint fk_group_members_group foreign key(group_id) references groups(id)
+CREATE TABLE if not exists `persistent_logins` (
+    `username` varchar(64) NOT NULL,
+    `series` varchar(64) NOT NULL PRIMARY KEY,
+    `token` varchar(64) NOT NULL,
+    `last_used` timestamp NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- insertion
 
 insert into `users`(`username`, `password`, `enabled`) 
 values 
-	('admin', '64b484d528cccca4dfe3036419f8a94f823ae76e8e1ffc1c245273827718469f83982b2e7a3f1e0d', true),
-	('user', 'de606efdad4166552eac27deba0fd33827839d9fe47d7d268c962fbe533b527d7a95c9c711125130', true),
-	('user_blocked', '60c7c3a707aff3cf7d2d5926685691bedd2e541db6f568597f55ba6f2e6598804232b87a0122988a', false);
+	('admin', '5182386375bf4ee4641ca484d17c879e0ce8d0a0f4613266e4cafe72e187548fd764a471a210ee7e', true),
+	('user', 'bff81665a6dd72c3d403059e081d1668bec4f44eb1674347e5793075a7be6c18dc605f0ef84f4b7f', true),
+	('user_blocked', '0bc86f42c366e70db440a80cbd873f8a8996b155090b92adbbb16ba81a836c642b01e2d79fb3d84a', true);
 
-insert into `groups`(`group_name`) 
-VALUES 
-	('Admins'),
-	('Users');
-	
-insert into `group_authorities`(`group_id`, `authority`) 
+INSERT INTO user_roles (username, role)
 VALUES
-	(1, 'ROLE_ADMIN'),
-	(2, 'ROLE_USER');
-	
-INSERT INTO `group_members` (`username`, `group_id`) 
-VALUES 
-	('admin', 1),
-	('user', 2),
-	('user_blocked', 2);
+	('admin', 'ROLE_ADMIN'),
+	('user', 'ROLE_USER');
