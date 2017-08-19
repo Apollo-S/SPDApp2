f30962d8ba236f2ebcd1a565233858707a446be5
apollo.saifullin@gmail.com <apollo.saifullin@gmail.com>
2017-08-19 13:37:43 +0300

    added class for persistent logins table instead script

---
+++ spdapp2/src/main/java/app/entity/PersistentLogin.java
@@ -0,0 +1,32 @@
+package app.entity;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+@Table(name = "persistent_logins")
+public class PersistentLogin {
+	
+	@Column(name = "username", length = 64, nullable = false)
+	private String username;
+	
+	@Id
+	@Column(name = "series", length = 64)
+	private String series;
+	
+	@Column(name = "token", length = 64, nullable = false)
+	private String token;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	@Column(name = "last_used", nullable = false)
+	private Date lastUsed;
+	
+	
+
+}
