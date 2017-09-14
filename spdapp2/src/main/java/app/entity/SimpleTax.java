8129d5c9eb94aa48f1f679b284121c31ad8a2d2f
apollo.saifullin@gmail.com <apollo.saifullin@gmail.com>
2017-09-15 00:34:41 +0300

    column 'value' is not nullable, @Temporal annot. in column 'date_start'

--- spdapp2/src/main/java/app/entity/SimpleTax.java
+++ spdapp2/src/main/java/app/entity/SimpleTax.java
@@ -6,6 +6,8 @@ import java.util.Date;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
@@ -16,9 +18,10 @@ public class SimpleTax extends UrlEntity implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
-	@Column(name = "value", columnDefinition=DECIMAL_10_2_DEFAULT_0_00)
+	@Column(name = "value", nullable=false, columnDefinition=DECIMAL_10_2_DEFAULT_0_00)
 	private Double value;
 
+	@Temporal(TemporalType.DATE)
 	@Column(name = "date_start")
 	private Date dateStart;
 
