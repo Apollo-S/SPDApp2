8842e17836fbd17d0a924c8bc28a3b36ef259ed2
apollo.saifullin@gmail.com <apollo.saifullin@gmail.com>
2017-11-02 23:36:10 +0200

    queries moved to the entities

--- spdapp2/src/main/java/app/repository/CalculationRepository.java
+++ spdapp2/src/main/java/app/repository/CalculationRepository.java
@@ -1,10 +1,8 @@
 package app.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 import app.entity.Calculation;
 
 @Repository
@@ -14,48 +12,19 @@ public interface CalculationRepository extends JpaRepository<Calculation, Intege
 	static final String PARAM_SPECIFICATION_ID = "specificationId";
 	static final String PARAM_CALCULATION_ID = "calculationId";
 	static final String PARAM_ALIAS = "alias";
-	static final String FIND_CLOSING_BALANCE_OF_LAST_CALCULATION_BY_AGREEMENT_ID = "select COALESCE(sc.closing_balance, 0) " 
-			+ "from agreement a join specification s on s.agreement_id = a.id join specification_calculation sc on sc.specification_id = s.id "
-			+ "where a.id = :agreementId order by sc.id desc limit 1";
-	static final String FIND_ACTUAL_ESV_RATE_BY_CALCULATION_ID = "select e.value from ESVTax e "
-			+ "where e.dateStart = (select max(e.dateStart) from ESVTax e where e.dateStart <= ("
-			+ "select c.dateStart from Calculation c where c.id = ?1))";
-	static final String FIND_ACTUAL_SIMPLE_TAX_RATE_BY_CALCULATION_ID = "select s.value from SimpleTax s "
-			+ "where s.dateStart = (select max(s.dateStart) from SimpleTax s where s.dateStart <= ("
-			+ "select c.dateStart from Calculation c where c.id = :calculationId))";
-	static final String FIND_ACTUAL_BANK_COMISSION_RATE_BY_CALCULATION_ID = "select p.value from Payment p, PaymentType pt "
-			+ "where p.paymentType.id = pt.id and pt.isBankComission = true and p.spd.id = ("
-			+ "select a.spd.id from Agreement a, Specification s, Calculation c "
-			+ "where a.id = s.agreement.id and s.id = c.specification.id and c.id = :calculationId) and p.dateStart = ("
-			+ "select max(c.dateStart) from Calculation c where c.dateStart <= (select c.dateStart from Calculation c where c.id = :calculationId))";
-	static final String FIND_ACTUAL_RATE_BY_ALIAS_AND_CALCULATION_ID = "select coalesce(p.value,0) from payment p, payment_type pt where p.payment_type_id = pt.id and pt.alias = :alias and p.spd_id = ("
-			+ "SELECT a.spd_id FROM agreement a, specification s, specification_calculation c WHERE a.id = s.agreement_id AND s.id = c.specification_id and c.id = :calculationId) and p.date_start = ("
-			+ "select max(p.date_start) from payment p, payment_type pt where p.payment_type_id = pt.id and pt.alias = :alias and p.spd_id = ("
-			+ "SELECT a.spd_id FROM agreement a, specification s, specification_calculation c WHERE a.id = s.agreement_id AND s.id = c.specification_id and c.id = :calculationId) and p.date_start <= ("
-			+ "select max(c.date_start) from specification_calculation c where c.id = :calculationId))";
-	
+
 	public Integer findMaxCalculationNumberBySpecificationId(@Param(PARAM_SPECIFICATION_ID) Integer specificationId);
 	
 	public Double findSumOfCalculationsBySpecificationId(@Param(PARAM_SPECIFICATION_ID) Integer specificationId);
 	
-	@Transactional
-	@Query(value = FIND_CLOSING_BALANCE_OF_LAST_CALCULATION_BY_AGREEMENT_ID, nativeQuery = true)
 	public Double findClosingBalanceOfLastCalculationByAgreementId(@Param(PARAM_AGREEMENT_ID) Integer agreementId);
 	
-	@Transactional
-	@Query(FIND_ACTUAL_ESV_RATE_BY_CALCULATION_ID)
 	public Double findActualEsvRateByCalculationId(@Param(PARAM_CALCULATION_ID) Integer calculationId);
 
-	@Transactional
-	@Query(FIND_ACTUAL_SIMPLE_TAX_RATE_BY_CALCULATION_ID)
 	public Double findActualSimpleTaxRateByCalculationId(@Param(PARAM_CALCULATION_ID) Integer calculationId);
 
-	@Transactional
-	@Query(FIND_ACTUAL_BANK_COMISSION_RATE_BY_CALCULATION_ID)
 	public Double findActualBankComissionRateByCalculationId(@Param(PARAM_CALCULATION_ID) Integer calculationId);
 	
-	@Transactional
-	@Query(value = FIND_ACTUAL_RATE_BY_ALIAS_AND_CALCULATION_ID, nativeQuery = true)
 	public Double findActualRateByAliasAndCalculationId(@Param(PARAM_CALCULATION_ID) int calculationId, @Param(PARAM_ALIAS) String alias);
 	
 }
--- spdapp2/src/main/java/app/repository/CompanyAccountRepository.java
+++ spdapp2/src/main/java/app/repository/CompanyAccountRepository.java
@@ -1,32 +1,16 @@
 package app.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.transaction.annotation.Transactional;
 import app.entity.CompanyAccount;
 
 public interface CompanyAccountRepository extends JpaRepository<CompanyAccount, Integer> {
 	
 	static final String PARAM_AGREEMENT_ID = "agreementId";
 	static final String PARAM_SPECIFICATION_ID = "specificationId";
-	static final String FIND_ACTUAL_COMPANY_ACCOUNT_BY_AGREEMENT_ID = "select ca from CompanyAccount ca where ca.company.id = ("
-			+ "select a.company.id from Agreement a where a.id = :agreementId) and ca.dateStart = ("
-			+ "select max(ca.dateStart) from CompanyAccount ca where ca.company.id = ("
-			+ "select a.company.id from Agreement a where a.id = :agreementId) and ca.dateStart <= ("
-			+ "select a.dateStart from Agreement a where a.id = :agreementId))";
-	static final String FIND_ACTUAL_COMPANY_ACCOUNT_BY_SPECIFICATION_ID = "select ca from CompanyAccount ca where ca.company.id = ("
-			+ "select a.company.id from Agreement a, Specification s where a.id = s.agreement.id and s.id = :specificationId) and ca.dateStart = ("
-			+ "select max(ca.dateStart) from CompanyAccount ca where ca.company.id = ("
-			+ "select a.company.id from Agreement a, Specification s where a.id = s.agreement.id and s.id = :specificationId) and ca.dateStart <= ("
-			+ "select s.dateStart from Specification s where s.id = :specificationId))";
-	
-	@Transactional
-	@Query(FIND_ACTUAL_COMPANY_ACCOUNT_BY_AGREEMENT_ID)
+
 	public CompanyAccount findActualCompanyAccountByAgreementId(@Param(PARAM_AGREEMENT_ID) Integer agreementId);
 
-	@Transactional
-	@Query(FIND_ACTUAL_COMPANY_ACCOUNT_BY_SPECIFICATION_ID)
 	public CompanyAccount findActualCompanyAccountBySpecificationId(@Param(PARAM_SPECIFICATION_ID) Integer specificationId);
 
 }
