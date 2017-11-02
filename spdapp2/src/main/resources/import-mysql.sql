7566eadd30b8615ab75c893a8558960c1ba76bf9
apollo.saifullin@gmail.com <apollo.saifullin@gmail.com>
2017-11-02 23:39:18 +0200

    added DemoUser record

--- spdapp2/src/main/resources/import-mysql.sql
+++ spdapp2/src/main/resources/import-mysql.sql
@@ -7,7 +7,8 @@ INSERT INTO `users` (`id`, `username`, `password`, `first_name`, `last_name`, `e
 VALUES
 	(1, 'admin', '5182386375bf4ee4641ca484d17c879e0ce8d0a0f4613266e4cafe72e187548fd764a471a210ee7e', NULL, NULL, NULL, 'ROLE_ADMIN', 1, 0),
 	(2, 'user', 'bff81665a6dd72c3d403059e081d1668bec4f44eb1674347e5793075a7be6c18dc605f0ef84f4b7f', NULL, NULL, NULL, 'ROLE_USER', 1, 0),
-	(3, 'user_blocked', '0bc86f42c366e70db440a80cbd873f8a8996b155090b92adbbb16ba81a836c642b01e2d79fb3d84a', NULL, NULL, NULL, 'ROLE_USER', 1, 0);
+	(3, 'user_blocked', '0bc86f42c366e70db440a80cbd873f8a8996b155090b92adbbb16ba81a836c642b01e2d79fb3d84a', NULL, NULL, NULL, 'ROLE_USER', 1, 0),
+	(4, 'DemoUser', '5182386375bf4ee4641ca484d17c879e0ce8d0a0f4613266e4cafe72e187548fd764a471a210ee7e', NULL, NULL, NULL, 'ROLE_ADMIN', 1, 0);
 
 TRUNCATE `spd`;
 INSERT INTO `spd` (`id`, `surname`, `firstname`, `lastname`, `alias`, `inn`, `passport`, `address_id`, `registration_info_id`, `optlock`)
